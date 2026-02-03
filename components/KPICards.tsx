3a52e95d5e2ba041695295fda04edd4e6a2fa7d2
jwecodes <124233072+jwecodes@users.noreply.github.com>
2026-02-04 00:45:57 +0530

    initial commit

--- components/KPICards.tsx
+++ components/KPICards.tsx
@@ -30,7 +30,7 @@ export default function KPICards() {
             ) : (
               <TrendingDown className="text-red-400" size={14} />
             )}
-            vs last period
+            vs last per
           </p>
         </div>
       ))}
